9fc802aaac6184f4cbd78a1b97d5b106d0fb9c0e
Owen Ripley <ojripley@gmail.com>
2019-09-17 13:01:34 -0400

    added a working version of eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertation Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let equal = true;
+
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        equal = true;
+      } else {
+        equal = false;
+      }
+    }
+  } else {
+    equal = false;
+  }
+  return equal;
+};
+
+// test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
