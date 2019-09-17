0f3ba4071fb36987ca2a0d4d6aeba486bd98f6db
Owen Ripley <ojripley@gmail.com>
2019-09-17 18:27:21 -0400

    added a working version of findKeyByValues.js

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const findKeyByValue = function(obj, value) {
+  for (let key in obj) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+};
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertation Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+// test cases
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
