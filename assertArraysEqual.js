496561a64b8ec677aae0c694ca0be83971f79839
Owen Ripley <ojripley@gmail.com>
2019-09-23 09:16:38 -0400

    refactored assertArraysEqual.js to use eqArrays properly, and also call its module via require

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,8 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(array1, array2) {
   
-  let equal = true;
-  
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        equal = true;
-      } else {
-        equal = false;
-      }
-    }
-  } else {
-    equal = false;
-  }
-  if (equal) {
+  if (eqArrays(array1, array2)) {
     console.log(`✅  Assertation Passed: ${array1} === ${array2}`);
   } else {
     console.log(`🛑  Assertation Failed: ${array1} !== ${array2}`);
@@ -24,3 +13,5 @@ assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
 assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should PASS
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
 assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should PASS
+
+module.export = assertArraysEqual;
