b19833d6ce5bfaf5b181b3a80627c7b35a1bab40
Owen Ripley <ojripley@gmail.com>
2019-09-23 12:40:31 -0400

    export module added to findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,20 +6,14 @@ const findKeyByValue = function(obj, value) {
   }
 };
 
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅  Assertation Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑  Assertation Failed: ${actual} !== ${expected}`);
-  }
-};
+// const bestTVShowsByGenre = {
+//   sciFi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama: "The Wire"
+// };
 
-const bestTVShowsByGenre = {
-  sciFi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire"
-};
+// // test cases
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 
-// test cases
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.export = findKeyByValue;
