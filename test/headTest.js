311723d18280b687e82cf438d305ffc707ed97b1
Owen Ripley <ojripley@gmail.com>
2019-09-23 09:39:48 -0400

    changed the test code for headTest.js to use M&C

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,17 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+// no need to use assertEqual if we are moving to chai
+// const assertEqual = require('../assertEqual');
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+// for using mocha and chai based test code...
+const assert = require('chai').assert;
+
+// my own test code
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), undefined);
+
+describe('#head', () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
